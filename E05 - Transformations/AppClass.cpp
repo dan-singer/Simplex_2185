67e2f9289ec4c5e31b1e70258ae23346c6530bbb
djs5435 <djs5435@IGMOH1370-18>
2019-02-12 15:17:51 -0500

    Start E05

---
+++ E05 - Transformations/AppClass.cpp
@@ -0,0 +1,51 @@
+#include "AppClass.h"
+void Application::InitVariables(void)
+{
+	//init the mesh
+	m_pMesh = new MyMesh();
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the arcball active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+
+	// Space invaders goes here
+	
+	
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+	m_pMeshMngr->AddGridToRenderList();
+	
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	SafeDelete(m_pMesh);
+
+	//release GUI
+	ShutdownGUI();
+}
